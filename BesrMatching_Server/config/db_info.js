debda5545de845fd60d3ec55e76e7af9ccd76fce
seok10319 <64138598+seok10319@users.noreply.github.com>
2020-10-22 22:46:40 +0900

    아이피수정

--- BesrMatching_Server/config/db_info.js
+++ BesrMatching_Server/config/db_info.js
@@ -6,7 +6,7 @@ const dbconnInfo = {
 		host: 'localhost',
 		port: '3306',
 		user: 'root',
-		password: 'root',
+		password: '129129',
 		database: 'best_matching', 
 		multipleStatements : true,
 	}//,
@@ -23,7 +23,7 @@ const dbconnInfo = {
 const dbconnection = {
 	init : function(){
 		var hostname = os.hostname();
-		if(hostname === 'SANGHUN'){
+		if(hostname === 'LAPTOP-JS'){
 			return mysql.createConnection(dbconnInfo.dev);	//로컬개발환경
         }
         // else{

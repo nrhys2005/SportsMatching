381bccac5c68e8be0db018f472022d44f6e723e1
nrhys2005 <nrhys2005@naver.com>
2020-10-25 21:29:19 +0900

    Update search.js

--- BesrMatching_Server/routes/match/search.js
+++ BesrMatching_Server/routes/match/search.js
@@ -4,9 +4,9 @@ const dbConObj = require('../../config/db_info');   //디비 정보 import
 const dbconn = dbConObj.init(); //sql 실행결과( results(배열 + json 형태)에 저장)
  
 //매치 검색
-router.get('/:search', function (req, res) {
+router.get('/h', function (req, res) {
     console.log('<<match/search>>');
-    var search = req.params.search;
+    var search = req.query.search;
     var search_data_array = [];
     let today = new Date(new Date().toISOString(). replace(/T/, ' ').replace(/\..+/, ''));
     
@@ -15,11 +15,11 @@ router.get('/:search', function (req, res) {
     console.log('Search = '+ search);
     if (search == "none") {
         sql = "select id, title, ground_name, DATE_FORMAT(start_time,'%Y-%m-%d %H:%i') as start_time, DATE_FORMAT(end_time,'%Y-%m-%d %H:%i') as end_time, cost, max_user, create_time, participants from best_matching.match,best_matching.matching_user where match.id = matching_user.match_id and match.end_time>=NOW() and matching_user.user_id <> ? and match.max_user <> match.participants and match.participants <> 0";
-        search_data_array.push( req.params.user_id);
+        search_data_array.push( req.query.user_id);
     }
     else {
         sql = "select id, title, ground_name, DATE_FORMAT(start_time,'%Y-%m-%d %H:%i') as start_time, DATE_FORMAT(end_time,'%Y-%m-%d %H:%i') as end_time, cost, max_user, create_time, participants from best_matching.match,best_matching.matching_user where match.id = matching_user.match_id and match.end_time>=NOW() and matching_user.user_id <> ? and match.max_user <> match.participants and match.participants <> 0 and match.title like ?";
-        search_data_array.push( req.params.user_id);
+        search_data_array.push( req.query.user_id);
         search_data_array.push('%' + search + '%');
     }
     dbconn.query(sql, search_data_array, function (err, rows, fields) {//DB connect

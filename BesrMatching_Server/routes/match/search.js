1fd9e889136eb7059499508b2edf6dca8a3accae
nrhys2005 <nrhys2005@naver.com>
2020-10-26 17:16:51 +0900

    Update search.js

--- BesrMatching_Server/routes/match/search.js
+++ BesrMatching_Server/routes/match/search.js
@@ -39,4 +39,39 @@ router.get('/', function (req, res) {
         }
     });
 });
+router.get('/team', function (req, res) {
+    console.log('<<match/search>>');
+    var search = req.query.search;
+    var search_data_array = [];
+    let today = new Date(new Date().toISOString(). replace(/T/, ' ').replace(/\..+/, ''));
+    
+    //var Data = JSON.parse(data); // JSON data 받음
+    var sql;
+    console.log('Search = '+ search);
+    if (search == "none") {
+        sql = "select id, title, ground_name, DATE_FORMAT(start_time,'%Y-%m-%d %H:%i') as start_time, DATE_FORMAT(end_time,'%Y-%m-%d %H:%i') as end_time, cost, max_user,min_user, create_time, participants from best_matching.team_match,best_matching.matching_user where match.id = matching_user.team_match_id and team_match.end_time>=NOW() and matching_user.user_id <> ? and team_match.max_user <> team_match.participants and team_match.participants <> 0";
+        search_data_array.push( req.query.user_id);
+    }
+    else {
+        sql = "select id, title, ground_name, DATE_FORMAT(start_time,'%Y-%m-%d %H:%i') as start_time, DATE_FORMAT(end_time,'%Y-%m-%d %H:%i') as end_time, cost, max_user, create_time, participants from best_matching.match,best_matching.matching_user where match.id = team_matching_user.team_match_id and team_match.end_time>=NOW() and matching_user.user_id <> ? and team_match.max_user <> team_match.participants and team_match.participants <> 0 and team_match.title like ?";
+        search_data_array.push( req.query.user_id);
+        search_data_array.push('%' + search + '%');
+    }
+    dbconn.query(sql, search_data_array, function (err, rows, fields) {//DB connect
+        if (!err) {
+            if (rows.length == 0) {
+                console.log('Query Select Success("result": "no find")');
+                res.json({ "result": "no find" });
+            }
+            else {
+                console.log(rows[0].start_time);
+                console.log('Query Select Success(result": "Success)');
+                res.json({ "result": "Success",match_info : rows });
+            }
+        } else {
+            console.log('Query Select Error : ' + err);
+            res.json({ "result": err });
+        }
+    });
+});
 module.exports = router;

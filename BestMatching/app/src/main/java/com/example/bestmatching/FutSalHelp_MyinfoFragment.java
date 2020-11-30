140ac39d549c6712a14dd689a8bb356c98cb4cb9
Hyeonseok <56476096+Hyeonseok0830@users.noreply.github.com>
2020-11-30 19:35:44 +0900

    Update FutSalHelp_MyinfoFragment.java
    내 정보 부분 세션을 확인하는 클래스 추가

--- BestMatching/app/src/main/java/com/example/bestmatching/FutSalHelp_MyinfoFragment.java
+++ BestMatching/app/src/main/java/com/example/bestmatching/FutSalHelp_MyinfoFragment.java
@@ -55,6 +55,8 @@ public class FutSalHelp_MyinfoFragment extends Fragment implements View.OnClickL
     public String e_age="";
     public String e_team="";
 
+
+
     public static FutSalHelp_MyinfoFragment newInstance() {
         return new FutSalHelp_MyinfoFragment();
     }
@@ -80,7 +82,7 @@ public class FutSalHelp_MyinfoFragment extends Fragment implements View.OnClickL
         change.setOnClickListener(this);
 
         new Get().execute(ip + "/Help/Myinfo?id="+send_id);
-        System.out.println(e_phone);
+        new Post_S().execute(ip+"/user/login/auto");
 
 
 
@@ -97,6 +99,91 @@ public class FutSalHelp_MyinfoFragment extends Fragment implements View.OnClickL
         }
 
     }
+    public class Post_S extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                //JSONObject를 만들고 key value 형식으로 값을 저장해준다.
+                JSONObject jsonObject = new JSONObject();
+                //jsonObject.put("user_id", "androidTest");
+                //jsonObject.put("name", "yun");
+
+
+
+                try{
+                    //URL url = new URL("http://192.168.25.16:3000/users");
+                    URL url = new URL(urls[0]);
+                    //연결을 함
+                    con = (HttpURLConnection) url.openConnection();
+
+                    con.setRequestMethod("POST");//POST방식으로 보냄
+                    con.setRequestProperty("Cache-Control", "no-cache");//캐시 설정
+                    con.setRequestProperty("Content-Type", "application/json");//application JSON 형식으로 전송
+                    con.setRequestProperty("Accept", "text/html");//서버에 response 데이터를 html로 받음
+                    con.setDoOutput(true);//Outstream으로 post 데이터를 넘겨주겠다는 의미
+                    con.setDoInput(true);//Inputstream으로 서버로부터 응답을 받겠다는 의미
+                    con.connect();
+
+                    //서버로 보내기위해서 스트림 만듬
+                    OutputStream outStream = con.getOutputStream();
+                    //버퍼를 생성하고 넣음
+                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outStream));
+                    writer.write(jsonObject.toString());
+                    writer.flush();
+                    writer.close();//버퍼를 받아줌
+
+                    //서버로 부터 데이터를 받음
+                    InputStream stream = con.getInputStream();
+
+                    reader = new BufferedReader(new InputStreamReader(stream));
+
+                    StringBuffer buffer = new StringBuffer();
+
+                    String line = "";
+                    while((line = reader.readLine()) != null){
+                        buffer.append(line);
+                    }
+
+                    return buffer.toString();//서버로 부터 받은 값을 리턴해줌 아마 OK!!가 들어올것임
+
+                } catch (MalformedURLException e){
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if(con != null){
+                        con.disconnect();
+                    }
+                    try {
+                        if(reader != null){
+                            reader.close();//버퍼를 닫아줌
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            //test1.setText(result);//서버로 부터 받은 값을 출력해주는 부분
+            try {
+                System.out.println("***"+result);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
     public class Post extends AsyncTask<String, String, String> {
 
         @Override
@@ -286,7 +373,7 @@ public class FutSalHelp_MyinfoFragment extends Fragment implements View.OnClickL
             if(team.getText().equals("null") || team.getText().equals(""))
                 team.setText("팀을 선택하세요");
 
-            System.out.println(result);
+
         }
 
     }

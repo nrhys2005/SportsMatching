830a112001d3515f90040b8ffcd30c6cb7ce65a9
nrhys2005 <nrhys2005@naver.com>
2020-12-01 17:57:51 +0900

    update config

--- BesrMatching_Server/config/config.js
+++ BesrMatching_Server/config/config.js
@@ -6,6 +6,17 @@ var config = {}
 dotenv.config({ path: path.join(__dirname, '.env') });
 const env = process.env
 
+
+mysql = {}
+mysql.development = {}
+mysql.development.username = env.MYSQLUSERNAME
+mysql.development.password = env.MYSQLPASSWORD
+mysql.development.database = env.MYSQLDATABASE
+mysql.development.host = env.MYSQLHOST
+mysql.development.port = env.MYSQLPORT
+mysql.development.dialect = env.MYSQLDIALECT
+
+
 redis = {}
 redis.development = {}
 redis.development.host = env.REDISHOST
@@ -14,5 +25,6 @@ redis.development.password = env.REDISPASSWORD
 redis.development.secret = env.REDISSECRET
 
 config.redis = redis
+config.mysql = mysql
 
 module.exports = config

060a509c7763cc89e98e2f4a27b62019ebf32323
nrhys2005 <nrhys2005@naver.com>
2020-12-08 22:22:23 +0900

    update sequelize(user)

--- BesrMatching_Server/routes/user/login.js
+++ BesrMatching_Server/routes/user/login.js
@@ -4,6 +4,7 @@ const crypto = require('crypto');
 const dbConObj = require('../../config/db_info');   //디비 정보 import
 const dbconn = dbConObj.init(); //sql 실행결과( results(배열 + json 형태)에 저장)
 const moment = require('moment');
+const models = require("../../database");
 
 require('moment-timezone')
 moment.tz.setDefault('Asia/Seoul')
@@ -14,58 +15,51 @@ router.post('/', function (req, res) {
         var login_id = req.body.id;
         var login_pw = req.body.pw;
         console.log('로그인 시도 아이디 : ' + login_id);
-        var sql = 'select * from user where id=?';
-        var param = [login_id];
-        dbconn.query(sql, param, function (err, rows) {
-            var find_pw = false;
-            if (err) {
-                console.log(err);
+        models.User.findOne({
+            where: {
+                id: login_id,
+            },
+        }).then((result) => {
+            if (result.length == 0) {
                 req.session.login = false;
                 res.json({
                     'result': 'No find'
                 });
             }
             else {
-                find_pw = true;
-                if (rows.length != 1) {
-                    console.log('존재하지 않는 계정입니다!');
-                    res.json({
-                        'result': 'No find'
-                    });
-                }
-                else {
-                    var salt = rows[0].salt;
-                    var pw = rows[0].pw;
-                    crypto.randomBytes(64, (err, buf) => {
-                        crypto.pbkdf2(login_pw, salt, 100000, 64, 'sha512', (err, key) => {
-                            var code;
-                            if (key.toString('base64') === pw) {
-                                code = 'Success';
-                                console.log('로그인 성공! ' + login_id + '님 환영합니다!');
-                                req.session.user_id = login_id;
-                            }
-                            else {
-                                console.log(pw);
-                                code = 'No find'
-                                console.log('비밀번호가 틀렸습니다!');
-                            }
-                            res.json({
-                                'result': code
-                            });
+                var salt = rows[0].salt;
+                var pw = rows[0].pw;
+                crypto.randomBytes(64, (err, buf) => {
+                    crypto.pbkdf2(login_pw, salt, 100000, 64, 'sha512', (err, key) => {
+                        var code;
+                        if (key.toString('base64') === pw) {
+                            code = 'Success';
+                            console.log('로그인 성공! ' + login_id + '님 환영합니다!');
+                            req.session.user_id = login_id;
+                        }
+                        else {
+                            code = 'No find'
+                            console.log('비밀번호가 틀렸습니다!');
+                        }
+                        res.json({
+                            'result': code
                         });
                     });
-                }
+                });
             }
-        })
-    }
+        }).catch((err) => {
+            console.log(err);
+            res.send(false);
+        });
+    };
 });
 
 router.post('/auto', function (req, res) {
     if (req.session.user_id) {
         res.send(true);
-      } else {
+    } else {
         //없으면
         res.send(false);
-      }
+    }
 });
 module.exports = router;
--- BesrMatching_Server/routes/user/signup.js
+++ BesrMatching_Server/routes/user/signup.js
@@ -3,40 +3,28 @@ const router = express.Router();
 const crypto = require('crypto');
 const dbConObj = require('../../config/db_info');   //디비 정보 import
 const dbconn = dbConObj.init(); //sql 실행결과( results(배열 + json 형태)에 저장)
+const models = require("../../database");
 
 
 router.post('/', function (req, res) {
-
     console.log('<<signup>>');
-
-
-    var input_data_array = [];
-    input_data_array.push(req.body.id);// json->array
-    input_data_array.push(req.body.name);
-    //var buffer;
     crypto.randomBytes(64, (err, buf) => {
         crypto.pbkdf2(req.body.pw, buf.toString('base64'), 100000, 64, 'sha512', (err, key) => {
-            //crypto.pbkdf2(inputData.pw, buf.toString('base64'), 9000, 64, 'sha512', (err, key) => {
-            input_data_array.push(buf.toString('base64')); //salt
-            input_data_array.push(key.toString('base64')); //pwd
-            // console.log("buf = " +buf.toString('base64')); 
-            // console.log("key = " +key.toString('base64')); // 'dWhPkH6c4X1Y71A/DrAHhML3DyKQdEkUOIaSmYCI7xZkD5bLZhPF0dOSs2YZA/Y4B8XNfWd3DHIqR5234RtHzw=='
-            //buffer = buf.toString('base64');
-            // pw = buf.toString('base64')+key.toString('base64');
-
-            input_data_array.push(req.body.email);
-            console.log('input_data : ' + input_data_array); // 회원가입 내용 출력
-
-            var sql_insert = 'INSERT INTO best_matching.user (id, name, salt, pw, email) VALUES(?, ?, ?, ?, ?)';
-            dbconn.query(sql_insert, input_data_array, function (err, rows, fields) {//DB connect
-                if (!err) {
-                    console.log('Query insert success');
-                    res.json({ "result": "Success" });
-                } else {
-                    console.log('Query Error : ' + err);
-                    res.json({ "result": err });
-                }
-            });
+            salt = buf.toString('base64')
+            pwd = key.toString('base64')
+            models.User.create({
+                id :req.body.id,
+                name : req.body.name,
+                salt : salt,
+                pwd : pwd,
+                email : req.body.email, 
+              }).then(() => {
+                console.log('Query insert success');
+                res.json({ "result": "Success" });
+              }).catch((error) => {
+                console.log('Query Error : ' + error);
+                res.json({ "result": err });
+              });
         });
     });
     // crypto.pbkdf2("pass", "salt", 100000, 64, 'sha512', (err, key) => {
@@ -52,30 +40,29 @@ router.post('/check', function (req, res, next) {
     console.log('<<Signup/check>>');
 
     var find_id = req.body.id;
-    var sql = 'SELECT * FROM user WHERE id = ?';
-    var param = find_id;
-    dbconn.query(sql, param, function (err, rows, fields) {
-        console.log(rows);
-        if (!err) {
-            if (rows.length == 0) {
-                console.log('no duplication');
-                res.json({ "result": "no duplication" });
-            }
-            else if (rows[0].id == find_id) {
-                console.log('duplication');
-                res.json({ "result": "duplication" });
-            }
-            else {
-                console.log('no duplication');
-                res.json({ "result": "no duplication" });
-            }
-        } else {
-            res.send(err);
-            console.log(err);
+    models.User.findOne({
+        where: {
+            id: find_id,
+        },
+    }).then((result) => {
+        if (result.length == 0) {
+            req.session.login = false;
+            res.json({
+                'result': 'No find'
+            });
         }
+        else if (result[0].id == find_id) {
+            console.log('duplication');
+            res.json({ "result": "duplication" });
+        }
+        else {
+            console.log('no duplication');
+            res.json({ "result": "no duplication" });
+        }
+    }).catch((err) => {
+        res.send(false);
+        console.log(err);
     });
-
-
 });
 
 module.exports = router;

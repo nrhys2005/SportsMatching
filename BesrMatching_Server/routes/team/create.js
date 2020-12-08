d9f3a04f46ea7ff8dc912f7a6647242728ce4c45
nrhys2005 <nrhys2005@naver.com>
2020-12-08 22:45:27 +0900

    Update sequelize(team-create)

--- BesrMatching_Server/routes/team/create.js
+++ BesrMatching_Server/routes/team/create.js
@@ -2,47 +2,40 @@ const express = require('express');
 const router = express.Router();
 const dbConObj = require('../../config/db_info');   //디비 정보 import
 const dbconn = dbConObj.init(); //sql 실행결과( results(배열 + json 형태)에 저장)
+const models = require("../../database");
 
 //팀 생성
 router.post('/', function (req, res) {
     console.log('<<Team/create>>');
-    var result_code;
-    
-    var input_data_array = [];
-    input_data_array.push(req.body.team_name);// json->array
-    input_data_array.push(req.body.master_id);// json->array
-    input_data_array.push(req.body.phonenumber);
-    input_data_array.push(req.body.age_avg);
-    input_data_array.push(req.body.level);
-    input_data_array.push(req.body.location);
-    input_data_array.push(req.body.week);
-    input_data_array.push(req.body.comment);
-    input_data_array.push(1);
-
-    var sql_insert = 'INSERT INTO best_matching.team (team_name,master_id, phonenumber, age_avg, level, location,week,comment,member_count) VALUES(?, ?, ?, ?, ?, ?, ?, ?)';
-    dbconn.query(sql_insert, input_data_array, function (err, rows, fields) {//DB connect
-        if (!err) {
-            console.log('Query insert success');
-        } else {
-            console.log('Query Error : ' + err);
-        }
+    //INSERT INTO best_matching.team (team_name,master_id, phonenumber, age_avg, level, location,week,comment,member_count) VALUES(?, ?, ?, ?, ?, ?, ?, ?)
+    models.Team.create({
+        team_name: req.body.team_name,
+        master_id: req.body.master_id,
+        phonenumber: req.body.phonenumber,
+        age_avg: req.body.age_avg,
+        level: req.body.level,
+        location: req.body.location,
+        week: req.body.week,
+        comment: req.body.comment,
+        member_count: 1
+    }).then(() => {
+        console.log('Query insert success');
+    }).catch((error) => {
+        console.log('Query Error : ' + error);
     });
-
-    var sql_update = 'update best_matching.user set team_name = ? where id = ? ';
-    var update_data_array = [];
-    update_data_array.push(req.body.team_name);
-    update_data_array.push(req.body.master_id);
-    dbconn.query(sql_update, update_data_array, function (err, rows, fields) {//DB connect
-        if (!err) {
-            console.log('Query Update Success');
-            result_code = 200;
-            res.json({ "result": result_code });
-
-        } else {
-            console.log('Query Update Error : ' + err);
-            result_code = 404;
-            res.json({ "result": result_code });
+    //'update best_matching.user set team_name = ? where id = ? ';
+    models.User.update({
+        team_name: req.body.team_name,
+    }, {
+        where: {
+            id: req.body.master_id
         }
+    }).then(() => {
+        console.log('Query Update Success');
+            res.json({ "result": 200 });
+    }).catch((err) => {
+        console.log('Query Update Error : ' + err);
+            res.json({ "result": 404 });
     });
 });
 module.exports = router;
